f484261f43e5d6a9e9abd868b0836309ff43d875
Allora Blesse Lagura <allorablesse.lagura@gsfe.tupcavite.edu.ph>
2022-10-20 12:26:36 +0800

    jsignature django delete

--- gradclear_project/gradclear_app/models.py
+++ gradclear_project/gradclear_app/models.py
@@ -6,8 +6,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.validators import MinLengthValidator
 import datetime
-from jsignature.fields import JSignatureField
-
 
 class user_table(AbstractUser):
 
