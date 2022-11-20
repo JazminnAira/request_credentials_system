ac3175c66c95668784292b52c39593e62a27d870
Allora Blesse Lagura <allorablesse.lagura@gsfe.tupcavite.edu.ph>
2022-11-20 17:19:00 +0800

    fix grad form, clearance and request display form, on going button hover in fac list clear and grad

--- gradclear_project/gradclear_app/models.py
+++ gradclear_project/gradclear_app/models.py
@@ -461,7 +461,7 @@ class graduation_form_table(models.Model):
         return self.student_id
 
 class request_form_table(models.Model):
-    student_id = models.CharField(max_length=20, verbose_name="Student Id")
+    student_id = models.CharField(max_length=50, verbose_name="Student Id")
     name = models.CharField(max_length=100, verbose_name="Student Name")
     name2 = models.CharField(max_length=100, verbose_name="2nd Format Student Name")
     address = models.CharField(max_length=100, verbose_name="Address")
