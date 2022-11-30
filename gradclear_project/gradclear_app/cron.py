2cff8f2d6d8587f52db29338d217f8800fa639f4
JazminnAira <jazminnaira.terencio@gsfe.tupcavite.edu.ph>
2022-11-30 16:50:14 +0800

    changes in time in cronjob

--- gradclear_project/gradclear_app/cron.py
+++ gradclear_project/gradclear_app/cron.py
@@ -2,4 +2,4 @@ from gradclear_app.views import send_email_all
 
 def my_scheduled_job():
     send_email_all()
-    print("works")
+    print("Email notifications has been sent to the signatories.")
